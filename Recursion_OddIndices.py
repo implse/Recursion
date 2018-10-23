d03aa69eba1c4e8b57ca2ad6bde16229c97d6d23
Finite <finite.impulse@gmail.com>
2018-10-23 05:55:36 +0200

    add Odd Indices

--- Recursion_OddIndices.py
+++ Recursion_OddIndices.py
@@ -1,14 +1,30 @@
 # Write a recursive function that prints out the odd indices of an integer.
 
-def indices(arr, i):
+# Simple Solution
+def indices_A(arr, i):
   # Base case
   if len(arr) == i:
     return
   # Recursive case
   if i % 2 != 0:
     print(arr[i])
-  indices(arr, i+1)
+  indices_A(arr, i+1)
+
+
+# Optimize Solution - reduce recursice calls
+def indices_B(arr, i):
+  # Base case
+  if i >= len(arr):
+    return
+  # Recursive case
+  print(arr[i])
+  indices_B(arr, i+2)
+
 
 # Test
 a = [2, 4, 6, 8, 10, 12]
-indices(a, 0)
+
+# Indices A
+indices_A(a, 0)
+# Indices B start i = 1
+indices_B(a, 1)
