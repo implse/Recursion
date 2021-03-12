9128cfdbfe2f5f143090d0c8587537500b7ad929
finite <finite.impulse@gmail.com>
2021-03-12 08:16:38 +0100

    Add memoization

--- Recursion_factorial.py
+++ Recursion_factorial.py
@@ -5,10 +5,26 @@ def factorial(n):
     return n * factorial(n - 1) # Break into smaller problem(s)
 
 
+# Factorial Memoization
+def factorial_memo(n):
+    memo = dict()
+    def factorial(n):
+        if n == 0:
+            return 1
+        if n in memo.keys():
+            return memo[n]
+        return n * factorial(n - 1)
+    return factorial(n)
+
+
 # Iterative factorial
-def factorial(n):
+def factorial_iterative(n):
     product = 1
     while n > 0:
         product *= n
         n -= 1
     return product
+
+print(factorial(10))
+print(factorial_memo(10))
+print(factorial_iterative(10))
